bf7afc0fc754f7e8c5e6378fc47d966a58826778
Daniel Jackson <email@danieljackson.co.uk>
2023-02-08 11:14:54 +0000

    -

--- demo.mjs
+++ demo.mjs
@@ -1,22 +1,77 @@
 import QrCode from './qrcode.mjs';
-import { renderDebug, renderTextLarge, renderTextMedium, renderTextCompact, renderSvg } from './render-matrix.mjs';
+import fs from 'fs';
 
-let text = "Hello, World!";
-let qrOptions = {}
-let renderOptions = {};
+const programOptions = {
+    text: 'Hello, world!',
+    help: false,
+    output: 'medium',
+    uppercase: false,
+    file: null,
+}
+const qrOptions = {}
+const renderOptions = {};
 
-if (process.argv && process.argv.length > 2) text = process.argv[2];
-//console.log(text);
+let matchParams = true;
+const textParts = [];
+for (let i = 2; i < process.argv.length; i++) {
+    const argv = process.argv[i];
+    if (matchParams && argv.startsWith('-')) {
+        // Program options
+        if (argv == '--help') { programOptions.help = true; }
+        else if (argv.startsWith('--output:')) { programOptions.output = argv.split(':')[1]; }
+        else if (argv == '--debug-data') { programOptions.output = 'large'; renderOptions.segments = ['  ', '██', '▓▓']; } // █▓▒░
+        else if (argv == '--uppercase') { programOptions.uppercase = true; }
+        else if (argv == '--file') { programOptions.file = process.argv[++i]; }
+        // QR options
+        else if (argv.startsWith('--ecl:')) { 
+            qrOptions.errorCorrectionLevel = {
+                l: QrCode.ErrorCorrectionLevel.L,
+                m: QrCode.ErrorCorrectionLevel.M,
+                q: QrCode.ErrorCorrectionLevel.Q,
+                h: QrCode.ErrorCorrectionLevel.H,
+            }[argv.split(':')[1]]; 
+        }
+        else if (argv == '--fixecl') { qrOptions.optimizeEcc = false; }
+        else if (argv == '--version') { qrOptions.minVersion = qrOptions.maxVersion = parseInt(process.argv[++i]); }
+        else if (argv == '--mask') { qrOptions.maskPattern = parseInt(process.argv[++i]); }
+        else if (argv == '--quiet') { qrOptions.quiet = parseInt(process.argv[++i]); }
+        else if (argv == '--invert') { qrOptions.invert = true; }
+        // SVG renderer options
+        else if (argv == '--svg-point') { renderOptions.moduleSize = parseFloat(process.argv[++i]); }
+        else if (argv == '--svg-round') { renderOptions.moduleRound = parseFloat(process.argv[++i]); }
+        else if (argv == '--svg-finder-round') { renderOptions.finderRound = parseFloat(process.argv[++i]); }
+        else if (argv == '--svg-alignment-round') { renderOptions.alignmentRound = parseFloat(process.argv[++i]); }
+        // End of options
+        else if (argv == '--') matchParams = false;
+        else {
+            console.error('ERROR: Unknown parameter: ' + argv);
+            process.exit(1);
+        }
+    } else {
+        //matchParams = false;
+        textParts.push(argv);
+    }
+}
 
-const matrix = QrCode.generate(text, qrOptions);
+if (programOptions.help) {
+    console.log('USAGE: [--ecl:<l|m|q|h>] [--uppercase] [--invert] [--quiet 4] [--output:<large|medium|compact|svg>] [--file filename] <text>');
+    console.log('')
+    console.log('For --output:svg:  [--svg-point 1.0] [--svg-round 0.0] [--svg-finder-round 0.0] [--svg-alignment-round 0.0]');
 
-let output;
+    process.exit(1);
+}
 
-//output = renderDebug(matrix);
-//output = renderTextLarge(matrix);
-output = renderTextMedium(matrix);
-//output = renderTextCompact(matrix);
-//output = renderTextLarge(matrix, ['  ', '██', '▓▓']);  // █▓▒░
-//output = renderSvg(matrix, renderOptions);
-
-console.log(output);
+if (textParts.length > 0) {
+    programOptions.text = textParts.join(' ');
+}
+if (programOptions.uppercase) {
+    programOptions.text = programOptions.text.toUpperCase();
+}
+//console.log('output=' + programOptions.output + ' \"' + programOptions.text + '\"');
+const matrix = QrCode.generate(programOptions.text, qrOptions);
+const output = QrCode.render(programOptions.output, matrix, renderOptions);
+if (programOptions.file) {
+    fs.writeFileSync(programOptions.file, output);
+} else {
+    console.log(output);
+}
--- qrcode.mjs
+++ qrcode.mjs
@@ -785,7 +785,6 @@ export default class QrCode {
     constructor() {
     }
 
-
     static generate(text, userOptions) {
 
         // Generation options
@@ -795,8 +794,8 @@ export default class QrCode {
             maxVersion: QrCode.VERSION_MAX,
             optimizeEcc: true,
             maskPattern: null,
-            quiet: Matrix.QUIET_STANDARD,
-            invert: false,
+            quiet: Matrix.QUIET_STANDARD,   // only information for the renderer
+            invert: false,                  // only a flag for the renderer
         }, userOptions)
 
         // Allow either a single text string or an array of text strings likely to encode as different modes
@@ -857,5 +856,194 @@ export default class QrCode {
         return matrix;
     }
 
+    static render(mode, matrix, renderOptions) {
+        const renderers = {
+            'debug': renderDebug,
+            'large': renderTextLarge,
+            'medium': renderTextMedium,
+            'compact': renderTextCompact,
+            'svg': renderSvg,
+        };
+        if (!renderers[mode]) throw new Error('ERROR: Invalid render mode: ' + mode);
+        return renderers[mode](matrix, renderOptions);
+    }
+
 }
 
+
+function renderDebug(matrix, options) {
+    options = Object.assign({
+        segments: ['  ', '██'],
+        sep: '\n',
+    }, options || {});
+    const lines = [];
+    for (let y = -matrix.quiet; y < matrix.dimension + matrix.quiet; y++) {
+        const parts = [];
+        for (let x = -matrix.quiet; x < matrix.dimension + matrix.quiet; x++) {
+            let part = matrix.identifyModule(x, y);
+            const bit = matrix.getModule(x, y) ? !matrix.invert : matrix.invert;
+            const value = bit ? 1 : 0;
+            if (typeof part == 'undefined' || part === null) part = options.segments[value];
+            parts.push(part);
+        }
+        lines.push(parts.join(''));
+    }    
+    return lines.join(options.sep);
+}
+
+function renderTextLarge(matrix, options) {
+    options = Object.assign({
+        segments: ['  ', '██'],
+        sep: '\n',
+    }, options || {});
+    const lines = [];
+    for (let y = -matrix.quiet; y < matrix.dimension + matrix.quiet; y++) {
+        const parts = [];
+        for (let x = -matrix.quiet; x < matrix.dimension + matrix.quiet; x++) {
+            const bit = matrix.getModule(x, y) ? !matrix.invert : matrix.invert;
+            const value = bit ? 1 : 0;
+            // If an additional segment type is specified, use it to identify data modules differently
+            const chars = (options.segments.length >= 3 && bit && !matrix.identifyModule(x, y)) ? options.segments[2] : options.segments[value];
+            parts.push(chars);
+        }
+        lines.push(parts.join(''));
+    }    
+    return lines.join(options.sep);
+}
+
+function renderTextMedium(matrix, options) {
+    options = Object.assign({
+        segments: [' ', '▀', '▄', '█'],
+        sep: '\n',
+    }, options || {});
+    const lines = [];
+    for (let y = -matrix.quiet; y < matrix.dimension + matrix.quiet; y += 2) {
+        const parts = [];
+        for (let x = -matrix.quiet; x < matrix.dimension + matrix.quiet; x++) {
+            const upper = matrix.getModule(x, y) ? !matrix.invert : matrix.invert;
+            const lower = (y + 1 < matrix.dimension ? matrix.getModule(x, y + 1) : 0) ? !matrix.invert : matrix.invert;
+            const value = (upper ? 0x01 : 0) | (lower ? 0x02 : 0);
+            // '▀', '▄', '█' // '\u{0020}' space, '\u{2580}' upper half block, '\u{2584}' lower half block, '\u{2588}' block
+            const c = options.segments[value];
+            parts.push(c);
+        }
+        lines.push(parts.join(''));
+    }    
+    return lines.join(options.sep);
+}
+
+function renderTextCompact(matrix, options) {
+    options = Object.assign({
+        segments: [' ', '▘', '▝', '▀', '▖', '▌', '▞', '▛', '▗', '▚', '▐', '▜', '▄', '▙', '▟', '█'],
+        sep: '\n',
+    }, options || {});
+    const lines = [];
+    for (let y = -matrix.quiet; y < matrix.dimension + matrix.quiet; y += 2) {
+        const parts = [];
+        for (let x = -matrix.quiet; x < matrix.dimension + matrix.quiet; x += 2) {
+            let value = 0;
+            value |= (matrix.getModule(x, y) ? !matrix.invert : matrix.invert) ? 0x01 : 0x00;
+            value |= (((x + 1 < matrix.dimension) ? matrix.getModule(x + 1, y) : 0) ? !matrix.invert : matrix.invert) ? 0x02 : 0x00;
+            value |= (((y + 1 < matrix.dimension) ? matrix.getModule(x, y + 1) : 0) ? !matrix.invert : matrix.invert) ? 0x04 : 0x00;
+            value |= (((y + 1 < matrix.dimension) && (x + 1 < matrix.dimension) ? matrix.getModule(x + 1, y + 1) : 0) ? !matrix.invert : matrix.invert) ? 0x08 : 0x00;
+            let c = options.segments[value];
+            parts.push(c);
+        }
+        lines.push(parts.join(''));
+    }    
+    return lines.join(options.sep);
+}
+
+function escape(text) {
+    return text.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/\"/g, '&quot;').replace(/\'/g, "&apos;");
+}
+
+function renderSvg(matrix, options) {
+    options = Object.assign({
+        moduleRound: null,
+        finderRound: null,
+        alignmentRound: null,
+        white: false,    // Output an element for every module, not just black/dark ones but white/light ones too.
+        moduleSize: 1,
+    }, options || {});
+
+    const lines = [];
+    lines.push(`<?xml version="1.0"?>`);
+    // viewport-fill=\"white\" 
+    lines.push(`<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="currentColor" viewBox="${-matrix.quiet} ${-matrix.quiet} ${2 * matrix.quiet + matrix.dimension} ${2 * matrix.quiet + matrix.dimension}" shape-rendering="crispEdges">`);
+    lines.push(`<title>${escape(matrix.text)}</title>`);
+    //lines.push(`<desc>${escape(matrix.text)}</desc>`);
+    lines.push(`<defs>`);
+
+    // module data bit (dark)
+    lines.push(`<rect id="b" x="${-options.moduleSize / 2}" y="${-options.moduleSize / 2}" width="${options.moduleSize}" height="${options.moduleSize}" rx="${0.5 * (options.moduleRound || 0) * options.moduleSize}" />`);
+
+    // module data bit (light). 
+    if (options.white) { // Light modules as a ref to a placeholder empty element
+        lines.push(`<path id="w" d="" visibility="hidden" />`);
+    }
+
+    // Use one item for the finder marker
+    if (options.finderRound != null) {
+        // Hide finder module, use finder part
+        lines.push(`<path id="f" d="" visibility="hidden" />`);
+        if (options.white) lines.push(`<path id="fw" d="" visibility="hidden" />`);
+        lines.push(`<g id="fc"><rect x="-3" y="-3" width="6" height="6" rx="${3.0 * options.finderRound}" stroke="currentColor" stroke-width="1" fill="none" /><rect x="-1.5" y="-1.5" width="3" height="3" rx="${1.5 * options.finderRound}" /></g>`);
+        lines.push(`<g id="fc"><rect x="-3" y="-3" width="6" height="6" rx="${3.0 * options.finderRound}" stroke="currentColor" stroke-width="1" fill="none" /><rect x="-1.5" y="-1.5" width="3" height="3" rx="${1.5 * options.finderRound}" /></g>`);
+    } else {
+        // Use normal module for finder module, hide finder part
+        lines.push(`<use id="f" xlink:href="#b" />`);
+        if (options.white) lines.push(`<use id="fw" xlink:href="#w" />`);
+        lines.push(`<path id="fc" d="" visibility="hidden" />`);
+    }
+
+    // Use one item for the alignment marker
+    if (options.alignmentRound != null) {
+        // Hide alignment module, use alignment part
+        lines.push(`<path id="a" d="" visibility="hidden" />`);
+        if (options.white) lines.push(`<path id="aw" d="" visibility="hidden" />`);
+        lines.push(`<g id="ac"><rect x="-2" y="-2" width="4" height="4" rx="${2.0 * options.alignmentRound}" stroke="currentColor" stroke-width="1" fill="none" /><rect x="-0.5" y="-0.5" width="1" height="1" rx="${0.5 * options.alignmentRound}" /></g>`);
+    } else {
+        // Use normal module for alignment module, hide alignment part
+        lines.push(`<use id="a" xlink:href="#b" />`);
+        if (options.white) lines.push(`<use id="aw" xlink:href="#w" />`);
+        lines.push(`<path id="ac" d="" visibility="hidden" />`);
+    }
+
+    lines.push(`</defs>`);
+
+    for (let y = 0; y < matrix.dimension; y++) {
+        for (let x = 0; x < matrix.dimension; x++) {
+            const mod = matrix.identifyModule(x, y);
+            let bit = matrix.getModule(x, y);
+            // IMPORTANT: Inverting the output for SVGs will not be correct if a single finder pattern is used (it would need inverting)
+            if (matrix.invert) bit = !bit;
+            let type = bit ? 'b' : 'w';
+
+            // Draw finder/alignment as modules (define to nothing if drawing as whole parts)
+            if (mod == 'Fi' || mod == 'FI') { type = bit ? 'f' : 'fw'; }
+            else if (mod == 'Al' || mod == 'AL') { type = bit ? 'a' : 'aw'; }
+
+            if (bit || options.white) {
+                lines.push(`<use x="${x}" y="${y}" xlink:href="#${type}" />`);
+            }
+        }
+    }
+
+    // Draw finder/alignment as whole parts (define to nothing if drawing as modules)
+    for (let y = 0; y < matrix.dimension; y++) {
+        for (let x = 0; x < matrix.dimension; x++) {
+            const mod = matrix.identifyModule(x, y);
+            let type = null;
+            if (mod == 'FI') type = 'fc';
+            else if (mod == 'AL') type = 'ac';
+            if (type == null) continue;
+            lines.push(`<use x="${x}" y="${y}" xlink:href="#${type}" />`);
+        }
+    }
+
+    lines.push(`</svg>`);
+
+    const svgString = lines.join('\n');
+    return svgString;
+}
